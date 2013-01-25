f06af43310ea8850e040acaaf057ea2b1b517e20
Joe Frew <JoeFrew95@gmail.com>
2013-01-25 11:41:55 +0000

    Made a simple fetcher that fetches page content.
    Made a simple fetcher class that fetches page content from the internet.
    Simply downloads the content; it doesn't have any kind of timeout or
    anything.

---
+++ src/fetching/SimpleFetcher.java
@@ -0,0 +1,30 @@
+package fetching;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/*
+ * The simple fetcher is a basic fetcher that simply downloads the webpage from the desired URL.
+ */
+
+public class SimpleFetcher implements Fetcher{
+
+	@Override
+	public String fetchData(String pageURL) throws Exception {
+		
+		String pageData = "";
+		
+		URL url = new URL(pageURL);
+		
+		BufferedReader Reader = new BufferedReader(new InputStreamReader(url.openStream()));
+		
+		String line;
+		while((line = Reader.readLine()) != null){
+			pageData = pageData + line + "\n";
+		}
+		
+		return pageData;
+	}
+
+}
--- src/test/FetcherTester.java
+++ src/test/FetcherTester.java
@@ -1,6 +1,7 @@
 package test;
 
 import debug.Debug;
+import fetching.SimpleFetcher;
 import fetching.TestFetcher;
 
 public class FetcherTester {
@@ -16,6 +17,17 @@ public class FetcherTester {
 			System.out.println(pagedata);
 		}catch(Exception e){
 			Debug.println("FETCHERTESTER", "FetcherTester failed because an exception arose in TestFetcher.");
+			e.printStackTrace();
+		}
+		
+		System.out.print("\n\n\n\n\n\n");
+		Debug.println("FETCHERTESTER", "Testing the SimpleFetcher with URL 'http://www.utopiacomputers.co.uk/'.");
+		
+		try{
+			String pagedata1 = (new SimpleFetcher()).fetchData("http://www.utopiacomputers.co.uk/");
+			System.out.println(pagedata1);
+		}catch(Exception e){
+			Debug.println("FETCHERTESTER", "FetcherTester failed because an exception arose in SimpleTester.");
 			e.printStackTrace();
 		}
 		
